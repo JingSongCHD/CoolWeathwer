bfcf75fd182b7d70f1da141b971afa2147d53742
SongJing <1158940754@qq.com>
2020-10-10 15:52:15 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweathwer/db/County.java
@@ -0,0 +1,47 @@
+package com.example.coolweathwer.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * @Description: java类作用描述
+ * @Author: sj
+ * @CreateDate: 2020/10/10 15:39
+ */
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
