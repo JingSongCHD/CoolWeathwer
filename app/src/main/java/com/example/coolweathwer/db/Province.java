3a5f1f0cf21e9fb468a519f122fb5c76d935e971
SongJing <1158940754@qq.com>
2020-10-10 15:32:14 +0800

    新增文件

---
+++ app/src/main/java/com/example/coolweathwer/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.coolweathwer.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * @Description: java类作用描述
+ * @Author: sj
+ * @CreateDate: 2020/10/10 15:28
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
